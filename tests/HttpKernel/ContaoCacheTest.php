87a3ec43ab967decf4392a2850119351e0d0d6b6
Yanick Witschi <yanick.witschi@terminal42.ch>
2018-03-26 15:24:05 +0200

    Use toflar/psr6-symfony-http-cache-store for cache tagging support and auto-cleanup

--- tests/HttpKernel/ContaoCacheTest.php
+++ tests/HttpKernel/ContaoCacheTest.php
@@ -15,8 +15,11 @@ namespace Contao\ManagerBundle\Tests\HttpKernel;
 use Contao\ManagerBundle\HttpKernel\ContaoCache;
 use Contao\TestCase\ContaoTestCase;
 use FOS\HttpCache\SymfonyCache\Events;
+use FOS\HttpCache\SymfonyCache\PurgeListener;
+use FOS\HttpCache\SymfonyCache\PurgeTagsListener;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Terminal42\HeaderReplay\SymfonyCache\HeaderReplaySubscriber;
+use Toflar\Psr6HttpCacheStore\Psr6Store;
 
 class ContaoCacheTest extends ContaoTestCase
 {
@@ -52,5 +55,11 @@ class ContaoCacheTest extends ContaoTestCase
             ],
             $options
         );
+
+        $preInvalidateListeners = $dispatcher->getListeners(Events::PRE_INVALIDATE);
+        $this->assertInstanceOf(PurgeListener::class, $preInvalidateListeners[0][0]);
+        $this->assertInstanceOf(PurgeTagsListener::class, $preInvalidateListeners[1][0]);
+
+        $this->assertInstanceOf(Psr6Store::class, $cache->getStore());
     }
 }
