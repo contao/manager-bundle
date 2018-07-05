de9cc4504aae8d14508563eb357d89151077f443
Leo Feyer <github@contao.org>
2018-07-05 18:18:07 +0200

    Add a deprecation notice.

--- src/HttpKernel/ContaoKernel.php
+++ src/HttpKernel/ContaoKernel.php
@@ -204,6 +204,7 @@ class ContaoKernel extends Kernel implements HttpCacheProvider
         $loader->load(
             function (ContainerBuilder $container): void {
                 if ($container->hasParameter('prepend_locale')) {
+                    @trigger_error('Defining the "prepend_locale" parameter in the parameters.yml file has been deprecated and will no longer work in Contao 5. Define the "contao.prepend_locale" parameter in the config.yml or config_prod.yml instead.', E_USER_DEPRECATED);
                     $container->setParameter('contao.prepend_locale', $container->getParameter('prepend_locale'));
                 }
             }
